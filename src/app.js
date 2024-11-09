9a2b39ad09f0d1c21700cf6643d69c3c348470b6
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2024-11-09 10:38:37 +0530

    "Removed empty line, updated cors origin comment"

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import router from "./router.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
 
-
 dotenv.config(
     "./.env"
 );
@@ -12,6 +11,7 @@ const app = express()
 
 app.use(cors({
     origin: 'http://localhost:5173',
+    // origin: 'http://127.0.0.1:3002',
     credentials: true, 
 }));
 app.use(express.json());
